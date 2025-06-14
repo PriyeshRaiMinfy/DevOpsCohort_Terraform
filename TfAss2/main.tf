30cb6e1a8e19721cad08a62cd2c242bb10061fbe
PriyeshRaiMinfy <priysh.rai@minfytech.com>
2025-06-14 15:58:11 +0530

    Added Assignment1 Terraform code and S3 website config

--- TfAss2/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region  = "ap-south-1"
-  profile = "TerrafornUser"
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-priysh-001"  
-  # ye globally unique hai
-}
